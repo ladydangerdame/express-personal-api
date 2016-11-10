50aca14dede1b969c8a862227f61898cb3a233a9
ladydangerdame <turbofemme@gmail.com>
2016-11-10 14:34:10 -0800

    Initial set up

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api");
+mongoose.connect( process.env.MONGODB_URI || "https://fast-stream-94232.herokuapp.com/");
 
 // module.exports.Campsite = require("./campsite.js.example");
